5467800b61345e3ecd91c0caceb576b7f131557f
Rushell <rzavalaga@unsa.edu.pe>
2024-07-31 15:15:16 -0500

    Changes's Deza done

--- backend/app/domain/repositories/user_repository.py
+++ backend/app/domain/repositories/user_repository.py
@@ -1,15 +1,48 @@
 from app.domain.entities.user import db, User
-from app.domain.repositories.base_repository import BaseRepository
 
 class UserRepository:
 
     @staticmethod
     def get_all_users():
         return User.query.all()
-        # Obtiene todos los usuarios de la base de datos.
 
     @staticmethod
-    def add(user):
+    def add_user(user):
         db.session.add(user)
         db.session.commit()
-        # Añade un usuario a la base de datos y confirma la transacción.
+
+    @staticmethod
+    def update_user(user):
+        db.session.commit()
+
+    @staticmethod
+    def get_user_by_id(user_id):
+        return User.query.get(user_id)
+    
+    @staticmethod
+    def get_user_by_username(username):
+        return User.query.filter_by(username=username).first()
+    
+    @staticmethod
+    def get_user_by_email(email):
+        return User.query.filter_by(email=email).first()
+
+    @staticmethod
+    def delete_user(user_id):
+        user = User.query.get(user_id)
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            return True
+        return False
+    
+    @staticmethod
+    def get_user_by_username(username):
+        return db.session.query(User).filter_by(username=username).first()
+
+    @staticmethod
+    def verify_user(username, password):
+        user = UserRepository.get_user_by_username(username)
+        if user and user.password == password:
+            return user
+        return None
--- backend/app/domain/services/user_service.py
+++ backend/app/domain/services/user_service.py
@@ -1,10 +1,30 @@
 from app.domain.repositories.user_repository import User,UserRepository
+from werkzeug.security import generate_password_hash
+from datetime import datetime
 
 class UserService:
 
     @staticmethod
     def get_all_users():
         return UserRepository.get_all_users()
+
+    @staticmethod
+    def get_user_by_id(user_id):
+        return UserRepository.query.get(user_id)
+
+    @staticmethod
+    def get_user_by_username(username):
+        user = UserRepository.query.filter_by(username)
+        return user
+
+    @staticmethod
+    def get_user_by_email(email):
+        user = UserRepository.get_user_by_email(email)
+        return user
+
+    @staticmethod
+    def authenticate_user(username, password):
+        return UserRepository.verify_user(username, password)
 
     @staticmethod
     def create_user(username, first_name, last_name, birth_date, phone_number, gender, email, password):
@@ -16,7 +36,41 @@ class UserService:
             phone_number=phone_number,
             gender=gender,
             email=email,
-            password=password
+            password=generate_password_hash(password, method='sha256')
         )
         UserRepository.add(new_user)
         return new_user
+
+
+    @staticmethod
+    def update_user(user_id, data):
+        try:
+            usuario = User.query.get(user_id)
+            if not usuario:
+                return {'error': 'Usuario no encontrado'}
+
+            if 'username' in data:
+                usuario.username = data['username']
+            if 'first_name' in data:
+                usuario.first_name = data['first_name']
+            if 'last_name' in data:
+                usuario.last_name = data['last_name']
+            if 'birth_date' in data:
+                try:
+                    usuario.birth_date = datetime.strptime(data['birth_date'], '%Y-%m-%d')
+                except ValueError:
+                    return {'error': 'Formato de fecha inválido.'}
+            if 'phone_number' in data:
+                usuario.phone_number = data['phone_number']
+            if 'gender' in data:
+                usuario.gender = data['gender']
+            if 'email' in data:
+                usuario.email = data['email']
+            if 'password' in data:
+                usuario.password = generate_password_hash(data['password'], method='sha256')
+
+            UserRepository.update_user(usuario)
+            return usuario
+
+        except Exception as e:
+            return {'error': f'Ocurrió un error al actualizar el usuario: {str(e)}'}
