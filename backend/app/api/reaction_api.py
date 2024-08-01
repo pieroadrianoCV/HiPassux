117766ace50af6ea8c4a3c93db0ba2539b7476a3
Rushell <rzavalaga@unsa.edu.pe>
2024-08-01 01:08:26 -0500

    Reaction done!

--- backend/app/api/reaction_api.py
+++ backend/app/api/reaction_api.py
@@ -10,30 +10,61 @@ def get_reactions():
     reactions = ReactionService.get_all_reactions()
     return jsonify([reaction.to_dict() for reaction in reactions])
 
-@reaction_api.route('/<int:reaction_id>', methods=['GET'])
-def get_reaction(reaction_id):
-    reaction = ReactionService.get_reaction_by_id(reaction_id)
-    if reaction:
-        return jsonify(reaction.to_dict())
+@reaction_api.route('/post/<int:post_id>', methods=['GET'])
+def get_reactions_by_post(post_id):
+    reactions = ReactionService.get_reactions_by_post(post_id)
+    if reactions:
+        return jsonify([reaction.to_dict() for reaction in reactions])
+    return jsonify({'error': error_reaction}), 404
+
+@reaction_api.route('/comment/<int:comment_id>', methods=['GET'])
+def get_reactions_by_comment(comment_id):
+    reactions = ReactionService.get_reactions_by_comment(comment_id)
+    if reactions:
+        return jsonify([reaction.to_dict() for reaction in reactions])
     return jsonify({'error': error_reaction}), 404
 
 @reaction_api.route('/', methods=['POST'])
 def create_reaction():
     data = request.json
-    new_reaction = ReactionService.create_reaction(data)
+    new_reaction = ReactionService.create_reaction(
+        data.get('user_id'),
+        data.get('type'),
+        data.get('post_id'),
+        data.get('comment_id')
+    )
     return jsonify(new_reaction.to_dict()), 201
 
-@reaction_api.route('/<int:reaction_id>', methods=['PUT'])
-def update_reaction(reaction_id):
+@reaction_api.route('/post/<int:post_id>/user/<int:user_id>', methods=['PUT'])
+def update_reaction_by_post_and_user(post_id, user_id):
     data = request.json
-    updated_reaction = ReactionService.update_reaction(reaction_id, data)
+    updated_reaction, error_reaction = ReactionService.update_reaction_by_post_and_user(post_id, user_id, data)
+
     if updated_reaction:
-        return jsonify(updated_reaction.to_dict())
+        return jsonify(updated_reaction.to_dict()), 200
+
     return jsonify({'error': error_reaction}), 404
 
-@reaction_api.route('/<int:reaction_id>', methods=['DELETE'])
-def delete_reaction(reaction_id):
-    success = ReactionService.delete_reaction(reaction_id)
+@reaction_api.route('/comment/<int:comment_id>/user/<int:user_id>', methods=['PUT'])
+def update_reaction_by_comment_and_user(comment_id, user_id):
+    data = request.json
+    updated_reaction, error_reaction = ReactionService.update_reaction_by_comment_and_user(comment_id, user_id, data)
+
+    if updated_reaction:
+        return jsonify(updated_reaction.to_dict()), 200
+
+    return jsonify({'error': error_reaction}), 404
+
+@reaction_api.route('/<int:post_id>', methods=['DELETE'])
+def delete_reaction_in_post(post_id, user_id):
+    success, error_reaction = ReactionService.delete_reaction_by_post_and_user(post_id, user_id)
     if success:
-        return jsonify({'message': 'Reaction deleted successfully'})
+        return jsonify({'message': 'Reaction deleted successfully'}), 200
+    return jsonify({'error': error_reaction}), 404
+
+@reaction_api.route('/comment/<int:comment_id>/<int:user_id>', methods=['DELETE'])
+def delete_reaction_in_comment(comment_id, user_id):
+    success, error_reaction = ReactionService.delete_reaction_by_comment_and_user(comment_id, user_id)
+    if success:
+        return jsonify({'message': 'Reaction deleted successfully'}), 200
     return jsonify({'error': error_reaction}), 404
